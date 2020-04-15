dc8e565edaff7d47b9b49851aae4260a72f205ac
Fatir Ahmad <djfdat@Fatirs-MacBook-Pro.local>
2020-04-15 00:10:10 -0700

    Rought Implementation

--- sci-hub-scholar.js
+++ sci-hub-scholar.js
@@ -1,7 +1,8 @@
 print = console.log
 const CROSSREF_QUERY = 'https://api.crossref.org/works?query='
+const SCIHUB_QUERY = 'https://www.sci-hub.tw/'
 
-document.body.style.border = "5px solid red";
+// document.body.style.border = "5px solid red";
 
 const collectedLinks = document.querySelectorAll('h3>a')
 // print(collectedLinks)
@@ -16,15 +17,40 @@ collectedLinks.forEach(element => {
 		if (data.status == 'ok') {
 			// print(url)
 			print(data)
-			if (data.message.items[0].title.toString().toLowerCase() == element.innerText.toString().toLowerCase()) {
+			statusIcon = document.createElement("img")
+			// statusIcon.src = browser.extension.getURL("icons/alert-circle.svg")
+			// statusIcon.style.color = "orange"
+			// statusIcon.style.fill = "orange"
+			// statusIcon.style.stroke = "orange"
+			element.insertAdjacentElement("beforebegin", statusIcon)
+			// element.appendChild(statusIcon)
+			checkObject = data.message.items[0]
+			if (checkObject.title.toString().toLowerCase() == element.innerText.toString().toLowerCase()) {
 				print("_______________")
 				print("Match found")
-				print(data.message.items[0].title.toString().toLowerCase())
-				print(element.innerText.toString().toLowerCase())
+				print(checkObject.title.toString().toLowerCase())
+				// print(element.innerText.toString().toLowerCase())
+				print(element.href)
+				print("DOI: " + checkObject.DOI)
 				print("_______________")
+				element.style.border = "2px solid blue";
+				element.href = SCIHUB_QUERY + checkObject.DOI
+				statusIcon.src = browser.extension.getURL("icons/check.svg")
+				// statusIcon.style.color = "green"
+				// statusIcon.style.fill = "green"
+				// statusIcon.style.stroke = "green"
 			}
 			else {
-
+				// print("_______________")
+				// print("Match not found")
+				// print(checkObject.title.toString().toLowerCase())
+				// print(element.innerText.toString().toLowerCase())
+				// print("_______________")
+				element.style.border = "2px solid red";
+				statusIcon.src = browser.extension.getURL("icons/x.svg")
+				// statusIcon.style.color = "red"
+				// statusIcon.style.fill = "red"
+				// statusIcon.style.stroke = "red"
 			}
 		}
 	})
